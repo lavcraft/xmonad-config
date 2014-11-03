10361f2b4d5b59fafa61f8e946e1fcc86a5485f0
Tolkachev Kirill <tolk.kv@gmail.com>
2014-11-03 19:20:39 +0300

    Fix install path

---
+++ install-02-apps.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+yaourt -S xorg xorg-xinit haskell-xmonad haskell-xmonad-contrib rxvt-unicode urxvt-perls-git dmenu numlockx xxkb \
+    slock openssh chromium rsync apache-ant maven gradle lftp unzip \
+    unrar tree alsa-utils flashplugin ntp ranger \
+    glances aspell aspell-en ispell bc colordiff colorsvn fasd dmidecode \
+    elinks htop iftop lesspipe highlight ncdu feh hwinfo pydf atool libcaca \
+    dzen2 mplayer tcl tk lnav ntfs-3g cloc zathura zathura-pdf-poppler dtrx \
+    dosfstools ntfsprogs ctags gvim fzf-git cabal-install the_silver_searcher \
+    wmctrl haskell-hlint fish kbdd xmobar
--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+cd $(dirname $BASH_SOURCE)
+
 mkdir -p ~/.xmonad
 
 mv -v ~/.xmonad/xmonad.hs ~/.xmonad/xmonad.old.hs 2> /dev/null
 
-ln -sf `pwd`/xmonad.hs ~/.xmonad/xmonad.hs
-
-ln -sf `pwd`/getvolume.sh ~/.xmonad/getvolume.sh
-ln -sf `pwd`/xmobar.hs ~/.xmonad/xmobar.hs
-ln -sf `pwd`/.xinitrc ~/.xinitrc
+ln -sf `pwd`/home/.xmonad/xmonad.hs ~/.xmonad/xmonad.hs
+ln -sf `pwd`/home/.xmonad/getvolume.sh ~/.xmonad/getvolume.sh
+ln -sf `pwd`/home/.xmonad/xmobar.hs ~/.xmonad/xmobar.hs
+ln -sf `pwd`/home/.xinitrc ~/.xinitrc
+ln -sf `pwd`/home/.gitconfig ~/.gitconfig
 
